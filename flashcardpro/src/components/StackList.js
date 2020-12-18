b61a9cbb079ec4988b55867680fcabc05ea934dd
abhirimal <abhirimal009@gmail.com>
2020-12-18 23:35:52 +0545

    redux example

---
+++ flashcardpro/src/components/StackList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import stacks from '../data/stacks.json';
+import { setStack } from '../actions';
+
+class StackList extends Component {
+    render() {
+        console.log('stacklist props', this.props);
+        return (
+            <div>
+            {
+                stacks.map( stack => {
+                    return (
+                        <Link
+                         to='/stack' 
+                        key={stack.id} 
+                        onClick={() => this.props.setStack(stack)}
+                        >
+                        <h4> {stack.title} </h4>
+                        </Link>
+                        )
+                })
+            }
+            </div>
+        )
+    }
+}
+
+
+export default connect(null,  { setStack })(StackList);
---
+++ flashcardpro/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import App from './components/App';
+import Stack from './components/Stack';
+import { setStack } from './actions';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()) );  //keeps track of how the store is doing
+store.dispatch(setStack({ id: 0, title: 'example', cards: []} ));
+
+ReactDOM.render(
+    <Provider store={store}> 
+        <BrowserRouter>
+        <Switch> 
+            <Route exact path='/' component={App} />
+            <Route path='/stack' component={Stack} />
+        </Switch>
+    </BrowserRouter>
+    </Provider>,
+    
+  document.getElementById('root')
+);
